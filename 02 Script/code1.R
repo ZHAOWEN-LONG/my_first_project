144622d7434ee7c7a5cf74dd2fe7704b886741c1
ZHAOWEN LONG <ZLONG003@E.NTU.EDU.SG>
2020-06-28 23:08:05 +0800

    to test its sync with Github

--- 02 Script/code1.R
+++ 02 Script/code1.R
@@ -6,3 +6,6 @@ abc<-mpg
 View(abc)
 mpg
 print("directory of project")
+print("pwd stands for Print Working Directory in Git Bash")
+print("ls list the content inside the current directory; cd .. means to go back to 1 level directory up; cd type sub folder name, will direct cd to nx folder apppointed; pwd to check again on the current folder location; sometimes need to use double quote to let cd recognize that the long string is a directory name to be changed - CD")
+
